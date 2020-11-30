abf0f4cabd6fd720f28bb315d67572f8117c8078
joe <1259813827@qq.com>
2020-11-30 18:16:52 +0800

    第一天的学习

--- src/com/joe/create/Way1Extends.java
+++ src/com/joe/create/Way1Extends.java
@@ -1,5 +1,36 @@
 package com.joe.create;
 
-public class Way1Extends {
+/**
+ * 线程变量不共享
+ * 
+ * @author hqf12
+ *
+ */
+public class Way1Extends extends Thread {
 
+	int i = 10;
+
+	public Way1Extends(String name) {
+		super.setName(name);
+	}
+
+	@Override
+	public void run() {
+		while (true) {
+			if (i < 1) {
+				break;
+			}
+			System.out.println(Thread.currentThread().getName() + "~" + i + "票");
+			i--;
+		}
+	}
+
+	public static void main(String[] args) {
+		Way1Extends way = new Way1Extends("窗口1");
+		Way1Extends way2 = new Way1Extends("窗口2");
+		Way1Extends way3 = new Way1Extends("窗口3");
+		way.start();
+		way2.start();
+		way3.start();
+	}
 }
